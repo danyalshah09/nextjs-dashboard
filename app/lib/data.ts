094342b679401fe571eb9efb4325399b00da1800
Danyal Shah <danyalshah1001@gmail.com>
2025-07-29 22:25:03 +0500

    done 3

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,19 +9,28 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// Cache the connection in development. This avoids recreating the connection on every HMR update.
+const globalForPostgres = global as unknown as {
+  conn: ReturnType<typeof postgres> | undefined;
+};
+
+const sql =
+  globalForPostgres.conn ?? postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+if (process.env.NODE_ENV !== 'production') globalForPostgres.conn = sql;
+
 
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
