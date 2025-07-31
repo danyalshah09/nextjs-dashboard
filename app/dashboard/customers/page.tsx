376c36e30857c24f09c65c72ce475d1c74c7f298
Danyal Shah <danyalshah1001@gmail.com>
2025-07-31 19:48:39 +0500

    await removed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -7,7 +7,7 @@ import Pagination from '@/app/ui/invoices/pagination';
 
 export default async function Page({searchParams,}:{searchParams?: {query?:string; page?: string;};}) {
     const query = searchParams?.query || '';
-    const currentPage =await  Number(searchParams?.page) || 1;
+    const currentPage = Number(searchParams?.page) || 1;
     const customers = await fetchFilteredCustomers(query,currentPage);
     const totalPages = await fetchCustomersPages(query);
     return (
